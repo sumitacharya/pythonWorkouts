1bfe0771063581398c32d9f97a441f9165b18efa
sumit <sumit.soome@gmail.com>
2015-09-26 11:07:31 +0200

    Uppercase to Lowercase and Lowercase to UpperCase converter added

---
+++ caseConv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# A small module that can be used in other programs
+
+# Coverts upper case to lower case
+def upper2Lower(upper):
+	if ord(upper)>64 and ord(upper)<91:
+		return chr(ord(upper)+32)
+	else :
+		return upper
+
+# Converts lower case to upper case
+def lower2Upper(lower):
+	if ord(lower)>=97 and ord(lower)<=122:
+		return chr(ord(lower)-32)
+	else :
+		return lower
+	
+
+# test cases
+print upper2Lower('A')
+print upper2Lower('z')
+
+print"\n\n", lower2Upper('s')  
+print lower2Upper('K')
+
